16060b427bed37bafe68ced499df1a23ef7efbd2
monkbroc <monkbroc@192.168.0.3>
2012-03-24 13:00:54 -0400

    Removed binaries from repository

---
+++ SendToCar/src/com/jvanier/android/sendtocar/DebugLogDummy.java
@@ -0,0 +1,15 @@
+package com.jvanier.android.sendtocar;
+
+public class DebugLogDummy implements DebugLog {
+
+	@Override
+	public void d(String s) {
+		
+	}
+
+	@Override
+	public void close() {
+		
+	}
+
+}
