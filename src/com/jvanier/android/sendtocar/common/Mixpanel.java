2bfea0eb40fab944a573c44b005e2736266d2e4f
Julien Vanier <jvanier@gmail.com>
2015-01-01 17:54:46 -0500

    Added missing drawables

---
+++ src/com/jvanier/android/sendtocar/common/Mixpanel.java
@@ -0,0 +1,17 @@
+package com.jvanier.android.sendtocar.common;
+
+import android.content.Context;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+public class Mixpanel {
+	private static MixpanelAPI MIXPANEL_API_INSTANCE;
+	
+	public static MixpanelAPI initializeSharedInstance(Context context, String token) {
+		MIXPANEL_API_INSTANCE = MixpanelAPI.getInstance(context, token);
+		return sharedInstance();
+	}
+
+	public static MixpanelAPI sharedInstance() {
+		return MIXPANEL_API_INSTANCE;
+	}
+}
