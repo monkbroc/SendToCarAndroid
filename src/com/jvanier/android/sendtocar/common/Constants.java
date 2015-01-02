4504d1da305b57aba1de8450f605fde3cbe1ba9c
Julien Vanier <jvanier@gmail.com>
2015-01-02 10:55:44 -0500

    Change constant to be consistent with iOS

--- src/com/jvanier/android/sendtocar/common/Constants.java
+++ src/com/jvanier/android/sendtocar/common/Constants.java
@@ -7,7 +7,6 @@ public class Constants {
 	public static final String MAKE_URL = BASE_URL + "makes#{0}";
 	public static final String OTHER_MAKES_URL = BASE_URL + "other_makes";
 	public static final String ISSUE_URL = BASE_URL + "issues/{0}.json";
-	public static final String ISSUE_VIEW_URL = BASE_URL + "issues#{0}";
 	public static final String MESSAGE_URL = BASE_URL + "issues#{0}";
 	public static final String CARS_URL = BASE_URL + "sendtocardata?hl={0}";
 	public static final String DEVELOPER_EMAIL = "sendtocar.app+android@gmail.com";
